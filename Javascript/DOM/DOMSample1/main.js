f9626fc122abb7be29666050ab2b46aeab2df074
husthunterpy01 <96186749+husthunterpy01@users.noreply.github.com>
2024-08-24 10:04:06 +0700

    Update FlexBox and BEM study

--- Javascript/DOM/DOMSample1/main.js
+++ Javascript/DOM/DOMSample1/main.js
@@ -12,3 +12,23 @@ console.log(headingNode1);
 // Get by tag name
 var headingNode2 = document.getElementsByTagName('h1');
 console.log(headingNode2);
+
+// Get by the query selector 
+var headingNode3 = document.querySelector('.heading1');
+console.log(headingNode3);
+
+//Get by the inbound query selector 
+var headingNode4 = document.querySelector(".box .heading-2");
+console.log(headingNode4);
+
+
+// Get the element by order
+var headingNode5 = document.querySelector('html .heading-2:first-child');
+console.log(headingNode5);
+
+var headingNode6 = document.querySelector('html .heading-2:nth-child(3)');
+console.log(headingNode6);
+
+// Get the element in terms of all 
+var headingNode7 = document.querySelectorAll('.heading1');
+console.log(headingNode7);
---
+++ Javascript/DOM/DOM_Properties/main.js
@@ -0,0 +1,3 @@
+var boxElement = document.querySelector('.box');
+boxElement.innerHTML = '<h3>Set liberation</h3>';
+console.log(boxElement)
